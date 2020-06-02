aa2977546f0d4c2d25285e7029e6eabdc66767e0
Timothy Quoe <tquoe001@ucr.edu>
2020-06-01 21:09:51 -0700

    first

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("This is loading correctly to your javascript!")
+
+$("#annotation_toggle").on("click", function(){
+
+    console.log("You want to turn on my annotations?");
+
+    $(".layer_4_2").toggle("on");
+});
+
+
