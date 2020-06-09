0a357af7851f9163660b8e8edf07646b3bf1c574
Timothy Quoe <tquoe001@ucr.edu>
2020-06-08 20:28:58 -0700

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+//Toggle on or off//
 console.log("This is loading correctly to your javascript!")
 
 $("#annotation_toggle").on("click", function(){
@@ -7,4 +9,13 @@ $("#annotation_toggle").on("click", function(){
     $(".layer_4_2").toggle("on");
 });
 
+//scroll to top button//
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
